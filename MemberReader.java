466b4f2e2e2c487f870fe72ebb92d74ae258d224
GoodieProp <xavierg980@gmail.com>
2019-11-13 15:50:33 -0600

    Added two new functions

--- MemberReader.java
+++ MemberReader.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -7,7 +11,58 @@
  *
  */
 public class MemberReader {
-	public static void main(String[] args) {
+	
+		public static Member parseMembers(String member) {
 			
-	}
+			String firstname;
+			String lastname;
+			int age;
+			int height;
+			int weight; 
+			int bp_syst;
+			int bp_dias;
+			String cancer;
+			String diabetes;
+			String alzheimers;
+			String parts[] = member.split("\t");
+			
+			firstname = parts[0];
+			lastname = parts[1];
+			age = Integer.parseInt(parts[2]);
+			height = Integer.parseInt(parts[3]);
+			weight = Integer.parseInt(parts[4]);
+			bp_syst = Integer.parseInt(parts[5]);
+			bp_dias = Integer.parseInt(parts[6]);
+			cancer = parts[7];
+			diabetes = parts[8];
+			alzheimers = parts[9];
+			
+			ArrayList<Member> members = new ArrayList<Member>();
+			
+			return new Member(firstname, lastname, age, height, weight, bp_syst, bp_dias, cancer, diabetes, alzheimers);
+		}
+	
+		public static ArrayList<Member> readMembers (String fname) {
+			
+			try {
+				/**
+				 * Creates a scanner for the file and also an arraylist that goes through the constructor class
+				 * Uses parseMembers to parse the info into an arraylist
+				 */
+				Scanner fsc = new Scanner(new File(fname));
+				String line = "";
+				ArrayList<Member> members = new ArrayList<Member>();
+				
+				while (fsc.hasNextLine()) {
+					line = fsc.nextLine();
+					members.add(parseMembers(fname));
+				}
+				
+				return members;
+				
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				return null;
+			}	
+		}
 }
