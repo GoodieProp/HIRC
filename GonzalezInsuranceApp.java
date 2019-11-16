70c9f10ad05f64b650b92ee4a6aae396c45bec63
GoodieProp <xavierg980@gmail.com>
2019-11-16 02:10:53 -0600

    Added 3rd and 4th choice functionality

--- GonzalezInsuranceApp.java
+++ GonzalezInsuranceApp.java
@@ -16,7 +16,7 @@ public class GonzalezInsuranceApp {
 		}
 		System.out.println(stars);
 	}
-	
+
 	// Welcome Message for the User
 	public static void printWelcome() {
 		printStars(45);
@@ -30,9 +30,9 @@ public class GonzalezInsuranceApp {
 				"  shared on a web-based data exchange.\r\n");
 		printStars(45);
 	}
-	
+
 	public static void showMenu() {
-		
+
 		//Options for the user
 		System.out.println("\nHere are your choices: ");
 		System.out.println("	1. List members");
@@ -43,14 +43,14 @@ public class GonzalezInsuranceApp {
 		System.out.println("	6. Save assessments as JSON");
 		System.out.println("	7. Exit");
 		System.out.print("Please enter your choice: ");
-			
+
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		int choice;
-		
+
 		//User inputs when choice is 2
 		String firstnameN;
 		String lastnameN;
@@ -62,76 +62,101 @@ public class GonzalezInsuranceApp {
 		String cancerN;
 		String diabetesN;
 		String alzheimersN;
-		
+
 		printWelcome();
-		
+
 		System.out.print("Enter name of member file: ");
 		String fn = sc.next();
-		ArrayList<Member> members = MemberReader.readMembers(fn);
-		
+		ArrayList<Member> members = MemberReader.loadMembers(fn);
+		System.out.printf("%s members were read.", members.size());
+
 		//c:/tempP/insurance.txt
-		
+
 		do {
 			showMenu();
 			choice = sc.nextInt();
-			
+			sc.nextLine();
+
 			if (choice == 1) {
+				// don't do listMembers cause it didn't work
+				// Print out the members
 				System.out.println("Here are the members: \n");
-				for (Member mems: members) {
-					System.out.println(mems);
-				}
+				MemberWriter.writeMembersToScreen(members);
 			} else if (choice == 2) {
-				
+
+				/**
+				 * 2nd choice is to add a new member
+				 */
+
+				// First and last name
 				System.out.println("Enter first and last name: ");
 				firstnameN = sc.next();
 				lastnameN = sc.next();
-				
-				
-				
+
+				// Age
 				System.out.println("Enter age: ");
 				ageN = sc.nextInt();
-				
+
+				// Height
 				System.out.println("Enter height in inches: ");
 				heightN = sc.nextInt();
-				
+
+				// Weight
 				System.out.println("Enter weight in pounds: ");
 				weightN = sc.nextInt();
-				
+
+				// Blood Pressure
 				System.out.println("Enter blood pressure (sys and dia): ");
 				bp_sys = Integer.parseInt(sc.next());
 				bp_dias = Integer.parseInt(sc.next());
-				
+
 				System.out.println("Has a family member had ...");
-				sc.nextLine();
+				sc.nextLine(); // sucks out the end of line marker
+
+				// Cancer
 				System.out.println("Cancer?: ");
-				cancerN = sc.nextLine();
-				
+				cancerN = sc.nextLine().toLowerCase();
+
+				// Diabetes
 				System.out.println("Diabetes?: ");
-				diabetesN = sc.nextLine();
-				
+				diabetesN = sc.nextLine().toLowerCase();
+
+				// Alzheimers
 				System.out.println("Alzheimers?: ");
-				alzheimersN = sc.nextLine();
-				
+				alzheimersN = sc.nextLine().toLowerCase();
+
 				Member mem = new Member(firstnameN, lastnameN, ageN, heightN, weightN, bp_sys, bp_dias, cancerN, diabetesN, alzheimersN);
-				
+
 				members.add(mem);
-				
-				
+
+				sc.nextLine();
 			} else if (choice == 3) {
-				
+				System.out.println("(T)ext, (B)inary, or (X)ML?: ");
+				String fileFormat = sc.next();
+
+				MemberWriter.writeMembersToFile(fileFormat, members);
+
 			} else if (choice == 4) {
-				
+
+				System.out.println("(T)ext, (B)inary, or (X)ML?: ");
+				String fileFormat = sc.next();
+
+				ArrayList<Member> member = MemberReader.readMembersFromFile(fileFormat);
+
 			} else if (choice == 5) {
-				
+
+				System.out.println("Here are the insurance assessments: ");
+				Assessor.assessMembers(members);
+
 			} else if (choice == 6) {
-				
+
 			} else if (choice == 7) {
 				System.out.println("   INSURANCE SCORE CARD");
 				System.out.println("	Thank You!");
 			}
-			
+
 		} while (choice != 7);
-		
+
 
 	}
 
